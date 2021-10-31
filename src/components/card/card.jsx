801642f9b57e912e9400977116bec390f2cbdf9f
Ju <j.semeniuk.v@gmail.com>
2021-10-31 10:35:56 +0200

    add styles

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,9 +5,7 @@ export const Card = (props) => {
   return (
     <div className="card-container">
       <img
-        src={`https://robohash.org/${
-          props.person.id + 6
-        }?set=set2&size=180x180`}
+        src={`https://robohash.org/${props.person.id}?set=set2&size=180x180`}
       />
       <p>{props.person.name}</p>
     </div>
